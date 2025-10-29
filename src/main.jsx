e2eb59c7d5674ef83aaa0561db1b90184aeb0f71
Thai Le <thai.lt194372@sis.hust.edu.vn>
2025-10-29 10:50:06 +0700

    add: speed insight

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {inject} from '@vercel/analytics';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { inject } from '@vercel/analytics';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 inject();
-import './index.css'
-import './styles.css'
-import App from './App.jsx'
+import './index.css';
+import './styles.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights />
     <App />
   </StrictMode>,
-)
+);
