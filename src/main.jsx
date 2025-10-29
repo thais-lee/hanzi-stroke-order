4d1988f1e4011e89cac8c31cceb59201bbe66e4c
Thai Le <thai.lt194372@sis.hust.edu.vn>
2025-10-29 10:42:56 +0700

    add: analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {inject} from '@vercel/analytics';
+inject();
 import './index.css'
 import './styles.css'
 import App from './App.jsx'
